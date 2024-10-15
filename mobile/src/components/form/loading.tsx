88778fa12df1546b035538c62c94545fc442cfce
Ju <112185575+juliana15paak@users.noreply.github.com>
2024-10-14 22:02:40 -0300

    Delete mobile directory

--- mobile/src/components/form/loading.tsx
+++
@@ -1,5 +0,0 @@
-import { ActivityIndicator } from "react-native";
-
-export function Loading(){
-    return <ActivityIndicator className="flex-1 bg-white items-center justify-center text-blueTheme-600" size={30}/>
-}
--- mobile/src/utils/form/genders.ts
+++
@@ -1,4 +0,0 @@
-export const genders = [
-    { sigla: 'F', name: 'Feminino' },
-    { sigla: 'M', name: 'Masculino' },
-  ];
